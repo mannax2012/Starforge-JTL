0010253b0f0d8b8bc52dad111e9068f4e668458e
mannax2012 <mannax@GamePC01>
2025-05-16 23:59:37 -0500

    [FIXED] Made entertainer craft grind make more sense

--- MMOCoreORB/bin/scripts/object/draft_schematic/instrument/instrument_fanfar.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/instrument/instrument_fanfar.lua
@@ -53,7 +53,7 @@ object_draft_schematic_instrument_instrument_fanfar = object_draft_schematic_ins
    factoryCrateType = "object/factory/factory_crate_generic_items.iff",
    
    xpType = "entertainer_healing", 
-   xp = 3000, 
+   xp = 4000, 
 
    assemblySkill = "instrument_assembly", 
    experimentingSkill = "general_experimentation", 
