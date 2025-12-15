595eefc4d2fc20b6fc76fa97285336f4eece8f52
mannax2012 <mannax@GamePC01.localdomain>
2025-12-15 17:58:34 -0600

    [FIXED] Part 2 of sample update - will return 1 if less than 1 yield from low density

--- MMOCoreORB/src/server/zone/managers/resource/resourcespawner/ResourceSpawner.cpp
+++ MMOCoreORB/src/server/zone/managers/resource/resourcespawner/ResourceSpawner.cpp
@@ -1026,6 +1026,7 @@ void ResourceSpawner::sendSampleResults(TransactionLog& trx, CreatureObject* pla
 		xpcap = 50;
 	}
 
+	/*
 	if (unitsExtracted < 2) {
 		// Send message to player about trace amounts
 		StringIdChatParameter message("survey", "trace_amount");
@@ -1034,6 +1035,11 @@ void ResourceSpawner::sendSampleResults(TransactionLog& trx, CreatureObject* pla
 		player->sendSystemMessage(message);
 		trx.abort() << message.toString();
 		return;
+	}
+	*/
+
+	if (unitsExtracted < 1){
+		unitsExtracted = 1;
 	}
 
 	// Send message to player about unit extraction
