cdb1180b7810ce41dd5cfd4fa6a135b815326827
mannax2012 <mannax@GamePC01.localdomain>
2026-04-15 20:45:39 -0500

    [FIXED] Some minor display errors in the SUI code for village time.

--- MMOCoreORB/bin/scripts/screenplays/village/village_sui.lua
+++ MMOCoreORB/bin/scripts/screenplays/village/village_sui.lua
@@ -9,11 +9,19 @@ function VillageSui:showMainPage(pPlayer)
 
 	local curPhase = VillageJediManagerTownship:getCurrentPhase()
 	local phaseID = VillageJediManagerTownship:getCurrentPhaseID()
-	local nextPhaseChange = VillageJediManagerTownship.getNextPhaseChangeTime()
+	local nextPhaseChange = VillageJediManagerTownship:getNextPhaseChangeTime()
 	local phaseTimeLeft = self:getPhaseDuration()
 
-	local suiPrompt = " \\#pcontrast1 " .. "Current Phase:" .. " \\#pcontrast2 " .. curPhase .. " (id " .. phaseID .. ")\n" .. " \\#pcontrast1 " .. "Current Server Time:" .. " \\#pcontrast2 " .. os.date("%c") .. "\n"
-	local suiPrompt = suiPrompt .. " \\#pcontrast1 " .. "Next Phase Change: " .. " \\#pcontrast2 " .. os.date("%c", nextPhaseChange)  .. "\n \\#pcontrast1 " .. "Phase Time Left: " .. " \\#pcontrast2 " .. phaseTimeLeft
+	local suiPrompt = " \\#pcontrast1 Current Phase: \\#pcontrast2 " .. curPhase .. " (id " .. phaseID .. ")\n" ..
+					  " \\#pcontrast1 Current Server Time: \\#pcontrast2 " .. os.date("%c") .. "\n"
+
+	if (nextPhaseChange ~= nil) then
+		suiPrompt = suiPrompt .. " \\#pcontrast1 Next Phase Change: \\#pcontrast2 " .. os.date("%c", nextPhaseChange) .. "\n"
+	else
+		suiPrompt = suiPrompt .. " \\#pcontrast1 Next Phase Change: \\#pcontrast2 unknown\n"
+	end
+
+	suiPrompt = suiPrompt .. " \\#pcontrast1 Phase Time Left: \\#pcontrast2 " .. phaseTimeLeft
 
 	local sui = SuiListBox.new("VillageGmSui", "mainCallback")
 	sui.setTitle("Starforge Village Time")
@@ -23,17 +31,31 @@ function VillageSui:showMainPage(pPlayer)
 end
 
 function VillageSui:getPhaseDuration()
-	local eventID = getServerEventID("VillagePhaseChange")
+	local nextPhaseChange = VillageJediManagerTownship:getNextPhaseChangeTime()
 
-	if (eventID == nil) then
-		return
+	if (nextPhaseChange == nil) then
+		return "unknown"
 	end
 
-	return self:getTimeString(getServerEventTimeLeft(eventID))
+	local timeLeft = nextPhaseChange - os.time()
+
+	if (timeLeft < 0) then
+		timeLeft = 0
+	end
+
+	return self:getTimeString(timeLeft * 1000)
 end
 
 function VillageSui:getTimeString(miliTime)
-	local timeLeft = miliTime / 1000
+	if (miliTime == nil) then
+		return "unknown"
+	end
+
+	if (miliTime < 0) then
+		miliTime = 0
+	end
+
+	local timeLeft = math.floor(miliTime / 1000)
 	local daysLeft = math.floor(timeLeft / (24 * 60 * 60))
 	local hoursLeft = math.floor((timeLeft / 3600) % 24)
 	local minutesLeft = math.floor((timeLeft / 60) % 60)
