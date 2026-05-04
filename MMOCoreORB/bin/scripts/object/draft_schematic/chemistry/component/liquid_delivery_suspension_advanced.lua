f8a9965b35ec942b94965f401b1b35902102218d
mannax2012 <mannax@GamePC01.localdomain>
2026-05-04 13:47:35 -0500

    [FIXED] Advanced Liquid Suspension xp = 25 to xp = 80 to mimic ABEC xp.

--- MMOCoreORB/bin/scripts/object/draft_schematic/chemistry/component/liquid_delivery_suspension_advanced.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/chemistry/component/liquid_delivery_suspension_advanced.lua
@@ -54,7 +54,7 @@ object_draft_schematic_chemistry_component_liquid_delivery_suspension_advanced =
 	factoryCrateType = "object/factory/factory_crate_chemicals.iff",
 
 	xpType = "medical",
-	xp = 25,
+	xp = 80,
 
 	assemblySkill = "medicine_assembly",
 	experimentingSkill = "medicine_experimentation",
