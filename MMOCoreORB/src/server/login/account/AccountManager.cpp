8bc3e2947fb767a8300b611651cfab48723651d3
mannax2012 <tincan2012@yahoo.com>
2026-06-22 17:03:04 -0500

    [UPDATE] AccountManager.cpp patch to help with session failure after log out. interation 1

--- MMOCoreORB/src/server/login/account/AccountManager.cpp
+++ MMOCoreORB/src/server/login/account/AccountManager.cpp
@@ -190,13 +190,14 @@ Reference<Account*> AccountManager::validateAccountCredentials(LoginClient* clie
 		sessionIdQuery << "SELECT a.active, a.username, a.password, a.salt, a.account_id, a.station_id, "
 			     "UNIX_TIMESTAMP(a.created), a.admin_level, IFNULL(s.session_id, '') AS session_id "
 			     "FROM accounts a, sessions s "
-			     "WHERE s.account_id = a.account_id AND s.session_id = '" << password << "'";
+			     "WHERE s.account_id = a.account_id AND s.session_id = '" << password << "'"
+			     " AND s.expires > NOW()";
 
 		if (!username.isEmpty()) {
 			sessionIdQuery << " AND a.username = '" << username << "'";
 		}
 
-		sessionIdQuery << "LIMIT 1;";
+		sessionIdQuery << " LIMIT 1;";
 
 		account = getAccount(sessionIdQuery.toString(), passwordStored, true);
 
