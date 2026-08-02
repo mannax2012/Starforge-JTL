8b2f8b3789fcf06acf09f247402377b6fb0ae7b5
mannax2012 <tincan2012@yahoo.com>
2026-08-02 17:33:34 -0500

    [ADDED] Unstable sabers to all_saber_scheamtics Loot table - Knight Trials Creatures added to lairs.

---
+++ MMOCoreORB/bin/scripts/mobile/lair/creature_lair/lok/lok_enraged_kimogila_lair_neutral_medium_boss_01.lua
@@ -0,0 +1,15 @@
+lok_enraged_kimogila_lair_neutral_medium_boss_01 = Lair:new {
+	mobiles = {
+		{"kimogila", 2},
+		{"kimogila_hatchling", 2}
+	},
+	bossMobiles = {{"enraged_kimogila", 1}},
+	spawnLimit = 14,
+	buildingsVeryEasy = {"object/tangible/lair/base/poi_all_lair_bones_large_evil_fire_small.iff"},
+	buildingsEasy = {"object/tangible/lair/base/poi_all_lair_bones_large_evil_fire_small.iff"},
+	buildingsMedium = {"object/tangible/lair/base/poi_all_lair_bones_large_evil_fire_small.iff"},
+	buildingsHard = {"object/tangible/lair/base/poi_all_lair_bones_large_evil_fire_small.iff"},
+	buildingsVeryHard = {"object/tangible/lair/base/poi_all_lair_bones_large_evil_fire_small.iff"},
+}
+
+addLairTemplate("lok_enraged_kimogila_lair_neutral_medium_boss_01", lok_enraged_kimogila_lair_neutral_medium_boss_01)
---
+++ MMOCoreORB/bin/scripts/mobile/lair/creature_lair/naboo/naboo_giant_peko_peko_eyrie_neutral_large.lua
@@ -0,0 +1,12 @@
+naboo_giant_peko_peko_eyrie_neutral_large = Lair:new {
+	mobiles = {{"giant_peko_peko", 1}},
+	bossMobiles = {{"peko_peko_albatross", 1}},
+	spawnLimit = 15,
+	buildingsVeryEasy = {"object/tangible/lair/base/poi_all_lair_rock_shelter_large_evil_fire_red.iff"},
+	buildingsEasy = {"object/tangible/lair/base/poi_all_lair_rock_shelter_large_evil_fire_red.iff"},
+	buildingsMedium = {"object/tangible/lair/base/poi_all_lair_rock_shelter_large_evil_fire_red.iff"},
+	buildingsHard = {"object/tangible/lair/base/poi_all_lair_rock_shelter_large_evil_fire_red.iff"},
+	buildingsVeryHard = {"object/tangible/lair/base/poi_all_lair_rock_shelter_large_evil_fire_red.iff"},
+}
+
+addLairTemplate("naboo_giant_peko_peko_eyrie_neutral_large", naboo_giant_peko_peko_eyrie_neutral_large)
