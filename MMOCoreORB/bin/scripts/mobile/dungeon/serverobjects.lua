64d4e2ad10824314435c65575af5c7224aecba01
mannax2012 <tincan2012@yahoo.com>
2026-08-02 18:38:46 -0500

    [FIXED] typo in serverobjects.lua preventing further loading of assets.

--- MMOCoreORB/bin/scripts/mobile/dungeon/serverobjects.lua
+++ MMOCoreORB/bin/scripts/mobile/dungeon/serverobjects.lua
@@ -3,6 +3,6 @@ includeFile("dungeon/death_watch_bunker/serverobjects.lua")
 includeFile("dungeon/geonosian_bio_lab/serverobjects.lua")
 includeFile("dungeon/ig88_factory/serverobjects.lua")
 includeFile("dungeon/exar_kun/serverobjects.lua")
-includeFile("dungeon/axkva_min/serverobjects.lua
+includeFile("dungeon/axkva_min/serverobjects.lua")
 includeFile("dungeon/star_destroyer/serverobjects.lua")
 includeFile("dungeon/warren/serverobjects.lua")
