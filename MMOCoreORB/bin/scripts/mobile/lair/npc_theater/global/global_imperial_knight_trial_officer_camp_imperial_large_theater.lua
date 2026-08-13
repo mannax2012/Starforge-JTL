577f849b6b62ee109c03cd68782c22cbd59b5b66
Voxxy <70905380+Voxxy-Flurry@users.noreply.github.com>
2026-08-13 08:34:56 +0200

    Update global_imperial_knight_trial_officer_camp_imperial_large_theater.lua
    Correct mobile template.

--- MMOCoreORB/bin/scripts/mobile/lair/npc_theater/global/global_imperial_knight_trial_officer_camp_imperial_large_theater.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/npc_theater/global/global_imperial_knight_trial_officer_camp_imperial_large_theater.lua
@@ -3,7 +3,7 @@ global_imperial_knight_trial_officer_camp_imperial_large_theater = Lair:new {
 		{"storm_commando", 5},
 		{"stormtrooper", 5}
 	},
-	bossMobiles = {{"novatrooper_elite_commander", 2}},
+	bossMobiles = {{"elite_novatrooper_commander", 2}},
 	spawnLimit = 15,
 	buildingsVeryEasy = {"object/building/poi/anywhere_imperial_base_large_1.iff"},
 	buildingsEasy = {"object/building/poi/anywhere_imperial_base_large_1.iff"},
