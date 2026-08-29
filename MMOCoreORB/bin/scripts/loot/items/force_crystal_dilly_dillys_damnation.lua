18043c27285c5d1cf0528791bd3951808b5372f6
mannax2012 <tincan2012@yahoo.com>
2026-08-29 17:04:08 -0500

    [ADDED] New named crytal types added for future loot table drops.

---
+++ MMOCoreORB/bin/scripts/loot/items/force_crystal_dilly_dillys_damnation.lua
@@ -0,0 +1,14 @@
+force_crystal_dilly_dillys_damnation = {
+	minimumLevel = 0,
+	maximumLevel = -1,
+	customObjectName = "Dilly Dilly's Damnation",
+	directObjectTemplate = "object/tangible/component/weapon/lightsaber/lightsaber_module_force_crystal.iff",
+	craftingValues = {
+		{"color",35,35,0},
+		{"quality",7,7,0},
+	},
+	customizationStringNames = {},
+	customizationValues = {}
+}
+
+addLootItemTemplate("force_crystal_dilly_dillys_damnation", force_crystal_dilly_dillys_damnation)
---
+++ MMOCoreORB/bin/scripts/loot/items/force_crystal_righteous_fury.lua
@@ -0,0 +1,14 @@
+force_crystal_righteous_fury = {
+	minimumLevel = 0,
+	maximumLevel = -1,
+	customObjectName = "Baylan's Righteous Fury",
+	directObjectTemplate = "object/tangible/component/weapon/lightsaber/lightsaber_module_force_crystal.iff",
+	craftingValues = {
+		{"color",36,36,0},
+		{"quality",7,7,0},
+	},
+	customizationStringNames = {},
+	customizationValues = {}
+}
+
+addLootItemTemplate("force_crystal_righteous_fury", force_crystal_righteous_fury)
